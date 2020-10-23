4af0d7b377583f40ab75bf121deee2a4abcd8186
Kriss <krissfrandsen@gmail.com>
2020-10-23 23:30:36 +0200

    make followers file

--- src/components/ReposContainer.js
+++ src/components/ReposContainer.js
@@ -2,13 +2,14 @@ import React from "react"
 import { Route } from "react-router-dom"
 import Overview from "./Overview"
 import Repos from "./Repos"
+import Followers from "./Followers"
 
 const ReposContainer = () => {
   return (
     <div className="reposContainer">
       <Route path="/overview" component={Overview} />
       <Route path="/repos" component={Repos} />
-      <Route path="followers" />
+      <Route path="followers" components={Followers} />
     </div>
   )
 }
